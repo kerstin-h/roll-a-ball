20ec5715586ff75db77bf0acc3dd12aa882c44d5
Kerstin Haustein <kerstin.haustein@brandwidth.co.uk>
2016-10-11 18:16:00 +0100

    Tutorial completed

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	// LateUpdate is called once her frame; but guaranteed to run after Update() has been processed
+	void LateUpdate () {
+		// follow our player with the camera
+		// place this in LateUpdate () so player is updated before camera position
+		transform.position = player.transform.position + offset;
+	}
+}
